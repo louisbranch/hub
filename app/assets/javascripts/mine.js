7e116ca5654c12050b39318e60762ff84f63659d
Luiz Branco <artfikan@gmail.com>
2011-09-22 17:38:39 -0300

    assets

--- app/assets/javascripts/mine.js
+++ app/assets/javascripts/mine.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     quotes();
-    checkLoginForm();
+    skillFilter();
 }
 
 function quotes(){
@@ -27,4 +27,38 @@ function setQuote(a){
   a.className ="img_active";
   quote_title.innerHTML = a.getAttribute("data-title");
   quote_block.innerHTML = a.getAttribute("data-quote");
+}
+
+function skillFilter(){
+  if(document.getElementById("skill_filters")){
+  var filter_block = document.getElementById("skill_filters");
+  var filter_links = filter_block.getElementsByTagName("a");
+      for(var i=0;i<filter_links.length;i++){
+        filter_links[i].onclick = function(){
+          for(var i=0;i<filter_links.length;i++){filter_links[i].className = "";}
+          filterSkillByType(this)
+        return false
+      }
+    }
+  }
+}
+
+function filterSkillByType(a){
+  a.className = "filter_active";
+  var filter_type = a.getAttribute("data-filter")
+  var skill_block = document.getElementById("class_skills");
+  var skills = skill_block.getElementsByTagName("article");
+  for(var i=0;i<skills.length;i++){
+    var all_skills = "all"
+    var skill_class = skills[i].getAttribute("class");
+    if(filter_type == skill_class){
+       skills[i].style.display = "";
+    }
+    else if (filter_type == all_skills){
+       skills[i].style.display = "";
+    }
+    else{
+       skills[i].style.display = "none";
+    }
+  }
 }
